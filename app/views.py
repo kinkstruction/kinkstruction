379fe260d707d24b88da2bda722e0c65261fd877
Kinkstruction Admin <admin@kinkstruction.com>
2014-07-20 04:05:03 -0500

    Set up some basic templates and separate dev and prod runicorn files

--- app/views.py
+++ app/views.py
@@ -1,7 +1,8 @@
 from app import app
+from flask import render_template, g, url_for
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, from kinkstruction.com!"
+    return render_template("index.html")
--- runicorn.sh
+++ runicorn.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-gunicorn run:app --log-file=- --bind="localhost:5000"
+gunicorn run:app --log-file=- --bind="localhost:80"
