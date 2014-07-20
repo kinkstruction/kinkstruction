cd4bf0d748a37e180a6353f75a1fe9dfe4541c8d
Kinkstruction Admin <admin@kinkstruction.com>
2014-07-20 04:55:59 -0500

    *mumblegrumble*

--- run-dev.py
+++ run-dev.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 
 from app import app
+from werkzeug.contrib.fixers import ProxyFix
+
+app.wsgi_app = ProxyFix(app.wsgi_app)
 
 if __name__ == "__main__":
 
--- run.py
+++ run.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 
 from app import app
+from werkzeug.contrib.fixers import ProxyFix
+
+app.wsgi_app = ProxyFix(app.wsgi_app)
 
 if __name__ == "__main__":
 
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=8080)
