d95dbe7e866eb3b1494294e4d9f56455abe89ca7
Kinkstruction Admin <admin@kinkstruction.com>
2014-07-27 09:53:04 -0500

    That was a bit easier than I thought...

--- app/forms.py
+++ app/forms.py
@@ -5,11 +5,23 @@ from app.models import User, Task
 
 
 # Can be used either for logging in or initially registering an account.
-class LoginRegisterForm(Form):
-    username = StringField('username'
-        #, validators=[InputRequired(), Length(min=2, max=256, message="Usernames must be between 2 and 256 characters")]
+class LoginForm(Form):
+    username = StringField('username',
+        # validators=[InputRequired(), Length(min=2, max=256, message="Usernames must be between 2 and 256 characters")]
             )
-    password = PasswordField('password'
-        #, validators=[InputRequired, Length(min=6, max=256, message="Passwords must be between 6 and 256 characters")]
+    password = PasswordField('password',
+        # validators=[InputRequired, Length(min=6, max=256, message="Passwords must be between 6 and 256 characters")]
             )
+    remember_me = BooleanField('remember_me', default=False)
+
+class SignUpForm(Form):
+    username = StringField('username',
+        # validators=[InputRequired(), Length(min=2, max=256, message="Usernames must be between 2 and 256 characters")]
+            )
+    password = PasswordField('password',
+        # validators=[InputRequired, Length(min=6, max=256, message="Passwords must be between 6 and 256 characters")]
+            )
+    email = StringField('email',
+        # validators=[InputRequired()]
+    )
     remember_me = BooleanField('remember_me', default=False)
