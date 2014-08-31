d64f24ac276a6d68b94a63d642fd9a1f681f89a4
Kinkstruction Admin <admin@kinkstruction.com>
2014-08-31 05:31:14 -0500

    word-wrap: break-word is a wonderful thing in CSS. Fixes #81.

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
 
+    $(".well").css("word-wrap", "break-word");
+    $(".panel-body").css("word-wrap", "break-word");
+
     $(".moment-js").each(function(i, ts){
         var $this = $(this);
         $this.html(moment.utc($this.html()).fromNow());
